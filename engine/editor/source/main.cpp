f43cea0cfe072e3be8ac029b90fa3a13e0db138c
qimenlang <510391246@qq.com>
2022-04-30 15:12:08 +0800

    3rdlib: add glad

--- engine/editor/source/main.cpp
+++ engine/editor/source/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <GLFW/glfw3.h>
 #include "editor.h"
 
 using namespace std;
--- engine/runtime/engine.cpp
+++ engine/runtime/engine.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
 #include "engine.h"
 namespace zeus{
     engine::engine() {};
     engine::~engine(){};
     void engine::run(){
+        glfwInit();
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         while (true)
         {
             std::cout<<"engine runing..."<<std::endl;
