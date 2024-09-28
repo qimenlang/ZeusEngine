7e5885f64c662e99b56ca87fd1601e192b922ff8
qimenlang <510391246@qq.com>
2024-09-28 11:23:43 +0800

    config clang-format, use llvm code style

--- engine/runtime/main.cpp
+++ engine/runtime/main.cpp
@@ -1,28 +1,29 @@
 
-#include "include/glad/glad.h"
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+
+#include "include/glad/glad.h"
 
 using namespace std;
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    std::cout<<"framebuffer_size_callback ["<<width<<","<<height<<"]"<<std::endl;
-    glViewport(0, 0, width, height);
-}  
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  std::cout << "framebuffer_size_callback [" << width << "," << height << "]"
+            << std::endl;
 
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
-        std::cout<<"processInput "<<GLFW_KEY_ESCAPE<<std::endl;
-        glfwSetWindowShouldClose(window, true);
-    }
+  glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    std::cout << "processInput " << GLFW_KEY_ESCAPE << std::endl;
+    glfwSetWindowShouldClose(window, true);
+  }
+}
 
 int main() {
   // init glfw
-  std::cout<<"Zeus Engine Start"<<std::endl;
+  std::cout << "Zeus Engine Start" << std::endl;
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -33,34 +34,32 @@ int main() {
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-     return -1;
+    return -1;
   }
   glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   //  glad: load all OpenGL function pointers
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-  {
-      std::cout << "Failed to initialize GLAD" << std::endl;
-       return -1;
-  } 
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
 
-  //render loop
-  while(!glfwWindowShouldClose(window))
-  {
-    //input 
+  // render loop
+  while (!glfwWindowShouldClose(window)) {
+    // input
     processInput(window);
-    
-    //rendering
+
+    // rendering
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    //check poll events & swap buffer
-    glfwPollEvents();    
+    // check poll events & swap buffer
+    glfwPollEvents();
     glfwSwapBuffers(window);
   }
-  
-  //glfw: terminate, clearing all previously allocated GLFW resources.
+
+  // glfw: terminate, clearing all previously allocated GLFW resources.
   glfwTerminate();
   return 0;
 }
--- engine/unit_test/example.cpp
+++ engine/unit_test/example.cpp
@@ -1,5 +1,4 @@
 #include <catch2/catch_test_macros.hpp>
-
 #include <cstdint>
 
 uint32_t factorial(uint32_t number) {
