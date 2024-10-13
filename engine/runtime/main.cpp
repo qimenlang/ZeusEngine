07a8ac925aab337387018f6d771fc94cac76af92
qimenlang <510391246@qq.com>
2024-10-13 21:40:48 +0800

    render two triangle

--- engine/runtime/main.cpp
+++ engine/runtime/main.cpp
@@ -1,9 +1,8 @@
-
-#include "include/glad/glad.h"
-
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+
+#include "include/glad/glad.h"
 
 using namespace std;
 
@@ -103,20 +102,30 @@ int main() {
   // set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f, // left
-      0.5f,  -0.5f, 0.0f, // right
-      0.0f,  0.5f,  0.0f  // top
+      0.5f,  0.5f,  0.0f,  // top right
+      0.5f,  -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f,  // bottom left
+      -0.5f, 0.5f,  0.0f   // top left
   };
-
-  unsigned int VBO, VAO;
+  unsigned int indices[] = {
+      // note that we start from 0!
+      0, 1, 3,  // first triangle
+      1, 2, 3   // second triangle
+  };
+  unsigned int VBO, VAO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
   // bind the Vertex Array Object first, then bind and set vertex buffer(s), and
   // then configure vertex attributes(s).
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
 
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
@@ -143,10 +152,10 @@ int main() {
 
     // draw our first triangle
     glUseProgram(shaderProgram);
-    glBindVertexArray(
-        VAO); // seeing as we only have a single VAO there's no need to bind it
-              // every time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(VAO);  // seeing as we only have a single VAO there's no
+                             // need to bind it every time, but we'll do so to
+                             // keep things a bit more organized
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     // glBindVertexArray(0); // no need to unbind it every time
 
     // check poll events & swap buffer
