7dcb3934d3efef51fd5d20c12d5d11bd879a62e0
qimenlang <510391246@qq.com>
2025-02-16 13:07:29 +0800

    fix projection matrix

--- engine/runtime/main.cpp
+++ engine/runtime/main.cpp
@@ -218,8 +218,9 @@ int main() {
     model =
         glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
     view = glm::translate(view, glm::vec3(0, 0, -3));
-    projection = glm::perspective(
-        glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_WIDTH, 0.1f, 100.0f);
+    projection =
+        glm::perspective(glm::radians(45.0f),
+                         (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
     ourShader.setMat4("model", model);
     ourShader.setMat4("view", view);
