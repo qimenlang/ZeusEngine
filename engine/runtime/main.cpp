5f0f9d9c627116d8b824333b9f64750f0c28e58b
qimenlang <510391246@qq.com>
2025-02-16 13:04:52 +0800

    enable depth test

--- engine/runtime/main.cpp
+++ engine/runtime/main.cpp
@@ -193,6 +193,7 @@ int main() {
   ourShader.setInt("texture1", 0); // 0是纹理单元索引即GL_TEXTURE0
   ourShader.setInt("texture2", 1);
 
+  glEnable(GL_DEPTH_TEST);
   // render loop
   while (!glfwWindowShouldClose(window)) {
     // input
@@ -200,7 +201,7 @@ int main() {
 
     // rendering
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // bind textures on corresponding texture units
     glActiveTexture(GL_TEXTURE0);
@@ -214,7 +215,8 @@ int main() {
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view = glm::mat4(1.0f);
     glm::mat4 projection = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-55.f), glm::vec3(1, 0, 0));
+    model =
+        glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
     view = glm::translate(view, glm::vec3(0, 0, -3));
     projection = glm::perspective(
         glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_WIDTH, 0.1f, 100.0f);
