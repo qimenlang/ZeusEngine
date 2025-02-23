dbce3f622ca18230cd1e777e258a0c12a024b562
qimenlang <510391246@qq.com>
2025-02-23 17:06:16 +0800

    add model class

---
+++ engine/runtime/Model.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class Mesh;
+struct Texture;
+class Shader;
+
+unsigned int TextureFromFile(const char *path, const std::string &directory,
+                             bool gamma = false);
+
+class Model {
+private:
+  std::vector<Mesh> meshes;
+  std::string directory;
+
+  std::vector<Texture> textures_loaded;
+
+  void loadModel(std::string path);
+  void processNode(aiNode *node, const aiScene *scene);
+  Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+  std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type,
+                                            std::string typeName);
+
+public:
+  Model(char *path) { loadModel(path); }
+  ~Model();
+  void Draw(Shader shader);
+};
