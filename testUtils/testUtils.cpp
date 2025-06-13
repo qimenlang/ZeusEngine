c1b9b604910c099c76575f7ea424816134742943
qimenlang <510391246@qq.com>
2025-06-13 08:31:13 +0800

    add event driven development doc

--- testUtils/testUtils.cpp
+++ testUtils/testUtils.cpp
@@ -1,11 +1,17 @@
+#include "cpp/SmartPointTest.hpp"
 #include "testPimpl/Widget.h"
 #include <iostream>
 
 int main() {
   std::cout << "test pimpl" << std::endl;
-  {
-    Widget widget;
-    widget.process();
-  }
+  // test Pimpl
+  // {
+  //   Widget widget;
+  //   widget.process();
+  // }
 
+  // test cpp shared ptr thread safe
+  {
+    testSharedPtrThreadSafe();
+  }
 }
