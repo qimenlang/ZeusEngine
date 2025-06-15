2272e569ee2cb6d82ba5543906659e61ce11b910
qimenlang <510391246@qq.com>
2025-06-15 15:37:20 +0800

    add cpp quiz 2

--- testUtils/cpp/cppQuiz/cppQuiz.cpp
+++ testUtils/cpp/cppQuiz/cppQuiz.cpp
@@ -4,22 +4,40 @@ namespace cppQuiz {
 
 // 显示特化模板
 // 放在头文件中时，会被多个包含该头文件的源文件编译，导致重复定义
-template <> void f(const int &i) { std::cout << __func__ << std::endl; }
+template <> void quiz1_1(const int &i) { PRINT_FUNC_NAME(); }
 
 void cppQuiz1() {
-  std::cout << __func__ << std::endl;
-  // i 是 int 类型的非常量左值
+  PRINT_FUNC_NAME(); // i 是 int 类型的非常量左值
   int i = 42;
 
   // 主模板是精确匹配、特化模板需要类型转换（int&->const int&
   // C++重载规则，优先选择不需要类型转换的精确匹配
-  f(i);
+  quiz1_1(i);
   // 核心结论
   // 1.模板特化不参与重载决议：编译器先选择最匹配的模板，再检查是否有特化
   // 2.常量性影响匹配：const 修饰符会导致不同的函数签名
   // 3.精确匹配优先：C++ 总是优先选择需要最少隐式转换的版本
-  f2(i);
+  quiz1_2(i);
 }
 
-void run() { cppQuiz1(); }
+void quiz2(const std::string &) { PRINT_FUNC_NAME(); }
+void quiz2(const char *) { PRINT_FUNC_NAME(); }
+void quiz2(const void *) { PRINT_FUNC_NAME(); }
+
+void cppQuiz2() {
+  PRINT_FUNC_NAME();
+  // 字符串标量并不是 std::string ,而是 const char[],若要匹配quiz_2(const
+  // string&),必须经历一次到string的隐式转换
+  quiz2("foo");
+  const char *bar = "bar";
+  quiz2(bar);
+  const void *bar2 = "bar2";
+  quiz2(bar2);
+  quiz2(std::string("string"));
+}
+
+void run() {
+  cppQuiz1();
+  cppQuiz2();
+}
 } // namespace cppQuiz
--- testUtils/cpp/cppQuiz/cppQuiz.hpp
+++ testUtils/cpp/cppQuiz/cppQuiz.hpp
@@ -1,19 +1,26 @@
 #pragma once
+#include <Common.h>
 #include <iostream>
 
 namespace cppQuiz {
-
+// Quiz1
 // 主模板，可以放在头文件中定义
-template <class T> void f(T &i) { std::cout << __func__ << std::endl; }
+template <class T> void quiz1_1(T &i) { PRINT_FUNC_NAME(); }
 // 显示特化模板，相当于普通函数，若头文件中定义，会被多个编译单元包含导致重复定义
-template <> void f(const int &i);
+template <> void quiz1_1(const int &i);
 
 // 主模板，可以放在头文件中定义
-template <class T> void f2(T &i) { std::cout << __func__ << std::endl; }
+template <class T> void quiz1_2(T &i) { PRINT_FUNC_NAME(); }
 // 显式特化模板（添加inline），inline允许函数在多个编译单元中重复定义，连接器会合并他们
-template <> inline void f2(const int &i) { std::cout << __func__ << std::endl; }
-
+template <> inline void quiz1_2(const int &i) { PRINT_FUNC_NAME(); }
 void cppQuiz1();
+
+// quiz2
+void quiz2(const std::string &);
+void quiz2(const char *);
+void quiz2(const void *);
+
+void cppQuiz2();
 
 void run();
 } // namespace cppQuiz
