5d2976e5e6317583b4985273b48872dc53a67949
LJX <jiaxiang.li@iris-view.com>
2025-07-15 19:42:44 +0800

    完善事件容器模板

--- engine/runtime/core/event.h
+++ engine/runtime/core/event.h
@@ -21,14 +21,35 @@ class Event<Ret(Args...)> {
    public:
     using RawType = Ret(Args...);
     using FuncType = std::function<RawType>;
-    void add(FuncType&& fn) { m_functions.emplace_back(std::move(fn)); }
+    using HandleType = uint32_t;
+
+    HandleType add(FuncType&& fn) {
+        ++m_handle;
+        m_functions.emplace_back(std::move(fn));
+        return m_handle;
+    }
+
+    HandleType add(FuncType& fn) {
+        ++m_handle;
+        m_functions.emplace_back(fn);
+        return m_handle;
+    }
+
+    void remove(HandleType handle) {
+        if (handle > 0 && handle <= m_functions.size()) {
+            m_functions.erase(m_functions.begin() + (handle - 1));
+        }
+    }
+
     void invoke(Args... args) {
         for (const auto& func : m_functions) {
             func(args...);
         }
     }
 
-    std::vector<FuncType> m_functions;
+    operator bool() const noexcept { return !m_functions.empty(); }
 
    private:
+    std::vector<FuncType> m_functions;
+    HandleType m_handle = 0;
 };
--- engine/unit_test/test_events.cpp
+++ engine/unit_test/test_events.cpp
@@ -2,12 +2,45 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-void test(int value) {}
+void test(int value){};
+TEST_CASE("test event template support function pointer", "[Event]") {
+    Event<void(int)> event;
+    event.add(test);
+    REQUIRE(bool(event));
+}
+
 TEST_CASE("test event template support lambda", "[Event]") {
     Event<void(int)> event;
     auto func = [](int value) {};
-
-    event.add(test);
     event.add(func);
-    REQUIRE(event.m_functions.size() == 2);
+    REQUIRE(bool(event));
+}
+
+TEST_CASE("test event template add and remove", "[Event]") {
+    Event<void(int)> event;
+    auto func = [](int value) {};
+    auto handle = event.add(func);
+    REQUIRE(bool(event));
+    event.remove(handle);
+    REQUIRE(!bool(event));
+}
+
+TEST_CASE("test event template invoke when add and remove", "[Event]") {
+    bool testInvoked = false;
+    auto func = [&](int value) { testInvoked = true; };
+
+    SECTION("event add and invoke") {
+        Event<void(int)> event;
+        event.add(func);
+        event.invoke(42);
+        REQUIRE(testInvoked);
+    }
+
+    SECTION("event remove and not invoke") {
+        Event<void(int)> event;
+        auto handle = event.add(func);
+        event.remove(handle);
+        event.invoke(42);
+        REQUIRE(testInvoked == false);
+    }
 }
