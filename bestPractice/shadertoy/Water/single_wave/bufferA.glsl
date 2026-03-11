54fdfee94e4780b34afb3209e6e254c06847e895
LJX <510391246@qq.com>
2026-03-11 17:59:49 +0800

    update rotor wash

---
+++ bestPractice/shadertoy/Water/single_wave/bufferA.glsl
@@ -0,0 +1,24 @@
+// shadertoy: bufferA
+#iKeyboard
+#iChannel0 "self"   // 读取自己的上一帧输出
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec3 cam;
+
+    if(iFrame == 0)
+        cam = vec3(0.0, 50.0, 100.0);
+    else
+         cam = texture2D(iChannel0, vec2(0.5)).xyz;
+
+    float speed = 0.2;
+
+    if(isKeyDown(Key_W)) cam.z -= speed; // W
+    if(isKeyDown(Key_S)) cam.z += speed; // S
+    if(isKeyDown(Key_A)) cam.x -= speed; // A
+    if(isKeyDown(Key_D)) cam.x += speed; // D
+    if(isKeyDown(Key_C)) cam.y -= speed; // C
+    if(isKeyDown(Key_E)) cam.y += speed; // E
+
+    fragColor = vec4(cam, 1.0);
+}
---
+++ bestPractice/shadertoy/Water/single_wave/bufferB.glsl
@@ -0,0 +1,22 @@
+// shadertoy: bufferB
+#iKeyboard
+#iChannel0 "self"   // 读取自己的上一帧输出
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec3 cam;
+
+    if(iFrame == 0)
+        cam = vec3(0.0, 120.0, 0.0);
+    else
+        cam = texture2D(iChannel0, vec2(0.5)).xyz;
+
+    float speed = 4.0;
+
+    if(isKeyDown(Key_T)) cam.y += speed; // T
+    if(isKeyDown(Key_G)) cam.y -= speed; // G
+    if(isKeyDown(Key_F)) cam.x -= speed; // F
+    if(isKeyDown(Key_H)) cam.x += speed; // H
+
+    fragColor = vec4(cam, 1.0);
+}
